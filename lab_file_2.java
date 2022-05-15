44a6d0b9e2ca5a4a19ad2c560c484ff18ca2f67b
Najib Hasan <hasan1807080@stud.kuet.ac.bd>
2022-05-16 00:25:35 +0600

    file 2

---
+++ lab_file_2.java
@@ -0,0 +1,55 @@
+public class Bank{
+    public String name;
+    public int account;
+    public String account_type;
+    public String branch_name;
+
+    public Bank() {
+    }
+
+    public Bank(String name, int account, String account_type, String branch_name) {
+        this.name = name;
+        this.account = account;
+        this.account_type = account_type;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAccount() {
+        return this.account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
+
+    public String getAccount_type() {
+        return this.account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+
+    public String getBranch_name() {
+        return this.branch_name;
+    }
+
+    public void setBranch_name(String branch_name) {
+        this.branch_name = branch_name;
+    }
+        this.branch_name = branch_name;
+    }
+    
+
+
+}
+
+
+
+
